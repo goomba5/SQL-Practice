79f644106442dc53e16bdf4d8b653144f6e4546f
Gary Henderson <hendergy@gmail.com>
2019-01-05 08:57:14 -0500

    update SpiderVerse director

--- class_practice/sqlReview.sql
+++ class_practice/sqlReview.sql
@@ -1,3 +1,23 @@
+-- CRUD Review Script
+
+-- select statement
 SELECT * FROM movie;
 
 SELECT * FROM actor;
+
+-- insert statement
+INSERT INTO movie (title, rating, year, director)
+VALUES('Spiderman: Into the SpiderVerse', 'PG', 2018, 'Phil Miller');
+
+SELECT * FROM movie
+WHERE year = 2018;
+
+-- update statement
+UPDATE movie
+SET director = 'Peter Ramsey'
+WHERE id = 5;
+
+SELECT * FROM movie
+WHERE id = 5;
+
+-- HOOOORAYYYYYYYYY

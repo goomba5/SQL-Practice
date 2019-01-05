c59b21dd65fd3aa517456a383cca3e5de757cab4
Gary Henderson <hendergy@gmail.com>
2019-01-05 09:13:16 -0500

    add second join statement

--- class_practice/sqlReview.sql
+++ class_practice/sqlReview.sql
@@ -21,3 +21,15 @@ SELECT * FROM movie
 WHERE id = 5;
 
 -- HOOOORAYYYYYYYYY
+
+-- join statement
+SELECT * FROM actor a
+	JOIN movie m
+    ON a.ID = m.ID;
+    
+SELECT m.title, m.year, a.firstName, a.lastName, c.characterName
+FROM credit c
+	JOIN actor a
+    ON a.ID = c.actorID
+    JOIN movie m
+    ON m.ID = c.movieID;
